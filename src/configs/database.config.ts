1d0210b52c9e88740892fecd05164d6f020c50c5
Narek Hovhannisyan <n.hovo@bk.ru>
2023-07-06 20:24:05 +0400

    added db variables

--- src/configs/database.config.ts
+++ src/configs/database.config.ts
@@ -1,16 +1,17 @@
-import "reflect-metadata"
-import { User } from "../entities/users.entity"
-import { Order } from "../entities/orders.entity"
-import { Product } from "../entities/products.entity"
+import 'reflect-metadata'
+import { User } from '../entities/users.entity'
+import { Order } from '../entities/orders.entity'
+import { Product } from '../entities/products.entity'
+import env from '../env/env.variables'
 
 export const DatabaseConfig = {
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "alpha23",
-    database: "postgres",
-    entities: [User, Product, Order],
-    synchronize: true,
-    migrations: ["src/migrations"],
+  host: env.databaseHost,
+  port: env.databasePort,
+  type: env.databaseType,
+  database: env.databaseName,
+  password: env.databasePassword,
+  username: env.databaseUsername,
+  entities: [User, Product, Order],
+  synchronize: true,
+  migrations: ['src/migrations'],
 }
--- src/env/env.variables.ts
+++ src/env/env.variables.ts
@@ -1,23 +1,35 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv'
 
-dotenv.config({});
+dotenv.config({})
 
 export interface EnvVariables {
-  port?: string;
-  firebaseApiKey?: string;
-  firebaseClientId?: string;
-  firebaseBucketUrl?: string;
-  firebaseProjectId?: string;
-  firebaseAuthDomain?: string;
+  port?: string
+  databaseType?: string
+  databaseHost?: string
+  databasePort?: string
+  databaseUsername?: string
+  databasePassword?: string
+  databaseName?: string
+  firebaseApiKey?: string
+  firebaseClientId?: string
+  firebaseBucketUrl?: string
+  firebaseProjectId?: string
+  firebaseAuthDomain?: string
 }
 
 const env: EnvVariables = {
   port: process.env.PORT,
+  databaseType: process.env.DATABASE_TYPE,
+  databaseHost: process.env.DATABASE_HOST,
+  databasePort: process.env.DATABASE_PORT,
+  databaseName: process.env.DATABASE_NAME,
+  databaseUsername: process.env.DATABASE_USERNAME,
+  databasePassword: process.env.DATABASE_PASSWORD,
   firebaseApiKey: process.env.FIREBASE_API_KEY,
   firebaseClientId: process.env.FIREBASE_CLIENT_ID,
   firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
   firebaseBucketUrl: process.env.FIREBASE_BUCKET_URL,
   firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
-};
+}
 
-export default env;
+export default env
