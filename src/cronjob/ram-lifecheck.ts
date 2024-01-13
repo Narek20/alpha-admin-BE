56c700614ff0ac8d4eca5b296fa5bf8c30f727bf
Narek Hovhannisyan <n.hovo@bk.ru>
2024-01-13 14:17:26 +0400

    modified cronjob

--- src/cronjob/ram-lifecheck.ts
+++ src/cronjob/ram-lifecheck.ts
@@ -1,4 +1,5 @@
 import os from 'os'
+import fs from 'fs'
 import cron from 'node-cron'
 
 const myTask = () => {
@@ -12,6 +13,23 @@ const myTask = () => {
   console.log(`Free Memory: ${freeMemory / 1024 / 1024} MB`)
   console.log(`Used Memory: ${usedMemory / 1024 / 1024} MB`)
   console.log(`Used Memory Percentage: ${usedMemoryPercentage.toFixed(2)}%`)
+  const date = new Date()
+  const result = `
+  Time: ${date}\n
+  Total Memory: ${totalMemory / 1024 / 1024} MB\n
+  Free Memory: ${freeMemory / 1024 / 1024} MB\n
+  Used Memory: ${usedMemory / 1024 / 1024} MB\n
+  Used Memory Percentage: ${usedMemoryPercentage.toFixed(2)}%\n
+  -------------------------------------------------------------
+  `
+
+  fs.appendFile('ram_usage_log.txt', result + '\n', (err) => {
+    if (err) {
+      console.error('Error writing to file:', err)
+    } else {
+      console.log('Result written to cpu_usage_log.txt')
+    }
+  })
 }
 
 cron.schedule('*/5 * * * *', myTask)
